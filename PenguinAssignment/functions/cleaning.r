02aa980b4d3e12d02c25a11d09320343a97678ea
iafhg2 <117743094+iafhg2@users.noreply.github.com>
2022-12-07 15:47:15 +0000

    Add files via upload

---
+++ PenguinAssignment/functions/cleaning.r
@@ -0,0 +1,41 @@
+## ---------------------------
+##
+## Script name: cleaning.r
+##
+## Purpose of script: definefunctions to clean data
+##
+## Author: ANONYMOUS
+##
+## Date Created: 2022-11-30
+##
+##
+## Notes:
+##   
+##
+## ---------------------------
+
+
+# 'cleaning' function: cleans column names, removes empty rows, and removes columns called comment and delta
+cleaning <- function(data_raw){
+  data_raw %>%
+    clean_names() %>%
+    remove_empty(c("cols", "rows")) %>%
+    select(-comments) %>%
+    select(-starts_with("delta"))
+}
+
+
+# 'correct_Adelie' function: decapitalises the 'Penguin' in Adelie Penguin, so that it is lowercase like in the other species:
+correct_Adelie <- function(data){
+  data1 <- data
+  data1$species <- str_replace(data$species, pattern = "Penguin", replacement = "penguin")
+  data1
+}
+  
+
+# 'remove_empty_culmen_length' function: subsets the data to only include the penguins that are not NA for the bill length
+remove_empty_culmen_length <- function(data_clean){
+  data_clean %>%
+    filter(!is.na(culmen_length_mm)) %>%
+    select(species, culmen_length_mm)
+}
---
+++ PenguinAssignment/functions/libraries.r
@@ -0,0 +1,27 @@
+## ---------------------------
+##
+## Script name: libraries.r
+##
+## Purpose of script: list libraries needed for analysis
+##
+## Author: ANONYMOUS
+##
+## Date Created: 2022-11-30
+##
+##
+## Notes:
+##   
+##
+## ---------------------------
+
+library(palmerpenguins)
+library(ggplot2)
+library(dplyr)
+library(janitor)
+library(tidyr)
+library(svglite)
+library(ragg)
+library(cowplot)
+library(magick)
+library(stringr)
+
